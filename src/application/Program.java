ca604455542dac252ade83419604e55f3ef58ecb
Juliana Prado <ju.pradoo@gmail.com>
2023-12-18 10:46:27 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,6 +18,8 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
+        System.out.println();
+
         System.out.println("=== TEST 2: seller findByDepartment ====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
@@ -25,14 +27,29 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println();
+
         System.out.println("=== TEST 3: seller findAll ====");
         list = sellerDao.findAll();
         for(Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println();
+
         System.out.println("=== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println();
+
+        System.out.println("=== TEST 5: update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+
     }
 }
